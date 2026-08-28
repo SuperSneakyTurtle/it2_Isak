57e69d31148c955230c00ee0f2ed6df6ab45e56e
SuperSneakyTurtle <isakaugustwaagbo@gmail.com>
2026-08-28 09:36:54 +0200

    Fysikk_koden_funker

--- Fysikk_strettball.py
+++ Fysikk_strettball.py
@@ -1,14 +1,46 @@
 import numpy as np
-import matplotlib as mp
+import matplotlib.pyplot as plt
 
-t: float = 10
-old_t: float = 5
-s: float = 2
-old_s: float = 0
-v: float = 0
+#Laster inn fila og henter tidsverdiene fra første kolonne og posisjonsverdiene fra andre kolonne.
+data = np.loadtxt("sprettball_data.txt")
+t = data[:,0]
+s = data[:,1]
 
-def calculate_velocity():
-    v = (s-old_s)/(t-old_t)
-    print(v)
+#Lager posisjonsgrafen.
+plt.figure(1)
+plt.plot(t,s)
+plt.grid()
+plt.title("Posisjon")
+plt.xlabel("$t$ / s")
+plt.ylabel("$s$ / m")
 
-    
+#Teller antall målinger og lager en liste for farten.
+n = len(t)
+v = np.zeros(n-1)
+
+#Regner ut gjennomsnittsfarten for hvert tidspunkt ved å bruke posisjonen i punktet før og etter.
+for i in range(1,n-1):
+    v[i] = (s[i+1]-s[i-1]) / (t[i+1]-t[i-1])
+
+#Lager fartsgrafen.
+plt.figure(2)
+plt.plot(t[1:n-1],v[1:n-1])
+plt.grid()
+plt.title("Fart")
+plt.xlabel("$t$ / s")
+plt.ylabel("$v$ / (m/s)")
+
+#Lager en liste for akselerasjonen.
+a = np.zeros(n-2)
+
+#Regner ut gjennomsnittsakselerasjonen for hvert tidspunkt ved å bruke farten i punktet før og etter.
+for i in range(2,n-2):
+    a[i] = (v[i+1]-v[i-1]) / (t[i+1]-t[i-1])
+
+plt.figure(3)
+plt.plot(t[2:n-2],a[2:n-2])
+plt.grid()
+plt.title("Akselerasjon")
+plt.xlabel("$t$ / s")
+plt.ylabel("$a$ / (m/s$^2$)")
+plt.show()
