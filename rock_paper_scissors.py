12b0d7df7009aa371188d38b4506ad9544a33fe7
SuperSneakyTurtle <isakaugustwaagbo@gmail.com>
2026-09-01 14:00:13 +0200

    Rock_paper_scissors WORKS!

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,31 +5,24 @@ player_input = input(str("WHRITE IN YOUR HAND!"))
 random_hand: None
 score: int = 0
 
-# def execute_functions() -> None:
-#     #handle_rules()
 
-# def randomize_hand() -> String:
-#     random_hand = hands[rd.randint(0, len(hands)-1)]
-#     return random_hand
+def randomize_hand() -> str:
+     random_hand = hands[rd.randint(0, len(hands)-1)]
+     return random_hand
 
-# #print(randomize_hand())
+print(randomize_hand())
+ 
 
-# def handle_rules() -> None:
-#     if player_input in hands:
-#         if player_input == randomize_hand:
-#             print("tie")
-#             add_to_score()
+def handle_rules() -> None:
+     if player_input in hands:
+         if player_input == randomize_hand:
+             print("tie")
 
-#         elif hands.index(player_input) == hands.index(random_hand()) +1 %3:
-#             print("you win")
-#             add_to_score()
+         elif hands.index(player_input) == hands.index(randomize_hand()) +1 %3:
+             print("you win")
+             has_won = True
         
-#         elif hands.index(player_input) == hands.index(random_hand()) -1 %3:
-#             print("you loose")
-#             add_to_score()
-#     else:
-#         print("invalid input")
-
-# def add_to_score():
-#     score += 1
-# execute_functions()
+         elif hands.index(player_input) == hands.index(randomize_hand()) -1 %3:
+             print("you loose")
+     else:
+         print("invalid input")
